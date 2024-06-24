8b576c46d52d1267389a0c1b98733f4ef4f5b1ff
James_ZY_L <zeyujames@gmail.com>
2024-06-23 21:39:58 -0700

    Update backup file

--- CS5200_Group_8_Inventory_system_backup.sql
+++ CS5200_Group_8_Inventory_system_backup.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `inventory_mgmt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `inventory_mgmt`;
 -- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
 --
 -- Host: localhost    Database: inventory_mgmt
--- Python_GUI/Inventory_system.sql
+++ Python_GUI/Inventory_system.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS inventory_mgmt;
 CREATE DATABASE  IF NOT EXISTS inventory_mgmt;
 USE inventory_mgmt;
 
--- 供应商
 CREATE TABLE Suppliers (
     supplier_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -10,29 +9,29 @@ CREATE TABLE Suppliers (
     address VARCHAR(255) NULL
 );
 
--- 产品
+
 CREATE TABLE Products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    selling_price DECIMAL(10, 2), -- 售价
-    safe_stock_level INT, -- 最低需要重新购入的库存值
-    healthy_stock_level INT, -- 正常需要的库存值
+    selling_price DECIMAL(10, 2),
+    safe_stock_level INT,
+    healthy_stock_level INT,
     shelf_space INT
 );
 
--- 目录表
+
 CREATE TABLE Catalog (
     catalog_id INT PRIMARY KEY AUTO_INCREMENT,
     supplier_id INT,
     product_id INT,
-    max_quantity INT, -- 最大生产数量
-    price DECIMAL(10, 2), -- 价格
+    max_quantity INT, 
+    price DECIMAL(10, 2),
     FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- 客户
+
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -40,27 +39,27 @@ CREATE TABLE Customers (
     address VARCHAR(255) NULL
 );
 
--- 仓库
+
 CREATE TABLE Warehouses (
     warehouse_id INT PRIMARY KEY AUTO_INCREMENT,
     location VARCHAR(255) NULL,
     capacity INT
 );
 
--- 仓库的库存
+
 CREATE TABLE Inventory (
     inventory_id INT PRIMARY KEY AUTO_INCREMENT,
     warehouse_id INT,
     product_id INT,
     quantity INT,
     shelf_space INT,
-    catalog_id INT, -- 连接到catalog中的item价格
+    catalog_id INT, 
 	FOREIGN KEY (catalog_id) REFERENCES Catalog(catalog_id),
     FOREIGN KEY (warehouse_id) REFERENCES Warehouses(warehouse_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- 客户订单
+
 CREATE TABLE SalesOrders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
@@ -71,7 +70,7 @@ CREATE TABLE SalesOrders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- order 的 product
+
 CREATE TABLE SalesOrderDetails (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
@@ -82,7 +81,7 @@ CREATE TABLE SalesOrderDetails (
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- po 给 supplier
+
 CREATE TABLE PurchaseOrders (
     po_id INT PRIMARY KEY AUTO_INCREMENT,
     supplier_id INT, 
@@ -94,7 +93,7 @@ CREATE TABLE PurchaseOrders (
 );
 
 
--- PO细节, 根据PO id, 和产品
+
 CREATE TABLE PurchaseOrderDetails (
     pod_id INT PRIMARY KEY AUTO_INCREMENT,
     po_id INT,
@@ -105,7 +104,7 @@ CREATE TABLE PurchaseOrderDetails (
     FOREIGN KEY (catalog_id) REFERENCES Catalog(catalog_id)
 );
 
--- 仓库间调货
+
 CREATE TABLE WarehouseTransfers (
     transfer_id INT PRIMARY KEY AUTO_INCREMENT,
     from_warehouse_id INT,
@@ -119,7 +118,7 @@ CREATE TABLE WarehouseTransfers (
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- All alerts
+
 CREATE TABLE Alerts (
     alert_id INT PRIMARY KEY AUTO_INCREMENT,
     entity_type VARCHAR(50),
@@ -130,7 +129,7 @@ CREATE TABLE Alerts (
 
 USE inventory_mgmt;
 
--- 插入供应商数据
+-- insert Supplier's data
 INSERT INTO Suppliers (supplier_id, name, contact_info, address) VALUES 
 (1, 'Supplier A', '123-456-7890', '123 East Supplier St.'),
 (2, 'Supplier B', '234-567-8901', '456 West Supplier Ave.'),
@@ -140,7 +139,7 @@ INSERT INTO Suppliers (supplier_id, name, contact_info, address) VALUES
 (6, 'Supplier F', '678-901-2345', '303 Uptown Supplier Way');
 
 
--- 插入产品数据
+-- INsert Product data
 INSERT INTO Products (product_id, name, description, selling_price, safe_stock_level, healthy_stock_level, shelf_space) VALUES
 (1, 'Laptop', 'A high-performance laptop with 16GB RAM and 512GB SSD.', 1200.00, 10, 50, 3),
 (2, 'Smartphone', 'Latest model smartphone with 8GB RAM and 128GB storage.', 800.00, 20, 100, 1),
@@ -194,7 +193,7 @@ INSERT INTO Products (product_id, name, description, selling_price, safe_stock_l
 (50, 'Pressure Washer', 'Electric pressure washer with 2000 PSI and multiple nozzles.', 180.00, 5, 25, 5);
 
 
--- 插入供应商产品数据
+-- Insert Catalog data
 INSERT INTO Catalog (catalog_id, supplier_id, product_id, max_quantity, price) VALUES
 (1, 1, 1, 30, 1150.00),
 (2, 2, 1, 50, 1200.00),
@@ -347,7 +346,7 @@ INSERT INTO Catalog (catalog_id, supplier_id, product_id, max_quantity, price) V
 (149, 5, 50, 20, 180.00),
 (150, 6, 50, 30, 200.00);
 
--- 插入客户数据
+-- Insert Customer Data
 INSERT INTO Customers (customer_id, name, contact_info, address) VALUES
 (1, 'John Smith', 'john.smith@example.com', '123 Maple St.'),
 (2, 'Jane Doe', 'jane.doe@example.com', '456 Oak Ave.'),
@@ -400,7 +399,7 @@ INSERT INTO Customers (customer_id, name, contact_info, address) VALUES
 (49, 'Wyatt Bell', 'wyatt.bell@example.com', '4646 Aspen St.'),
 (50, 'Penelope Cooper', 'penelope.cooper@example.com', '4747 Hickory Ave.');
 
--- 插入仓库数据
+-- Insert Warehouse Data
 INSERT INTO Warehouses (warehouse_id, location, capacity) VALUES
 (1, 'New York', 2000),
 (2, 'Los Angeles', 2500),
@@ -409,7 +408,7 @@ INSERT INTO Warehouses (warehouse_id, location, capacity) VALUES
 (5, 'Phoenix', 1900);
 
 
--- 插入库存数据
+-- Insert Inventory Data
 INSERT INTO Inventory (inventory_id, warehouse_id, product_id, quantity, shelf_space, catalog_id) VALUES
 -- Warehouse 1: Small stock
 (1, 1, 1, 5, 15, 1),
@@ -507,7 +506,7 @@ INSERT INTO Inventory (inventory_id, warehouse_id, product_id, quantity, shelf_s
 (89, 5, 30, 48, 240, 89);
 
 
--- 插入客户订单数据
+-- Insert Mock Sales Orders 
 INSERT INTO SalesOrders (order_id, customer_id, order_date, total_price, delivery_date, status) VALUES
 (1, 1, '2024-01-10', 2400, '2024-01-15', 'Delivered'),
 (2, 2, '2024-01-12', 1600, '2024-01-17', 'Delivered'),
@@ -561,7 +560,7 @@ INSERT INTO SalesOrders (order_id, customer_id, order_date, total_price, deliver
 (50, 50, '2024-04-17', 1100, '2024-04-24', 'Processing');
 
 
--- 插入购买详情数据
+-- Insert Mock PO detail
 INSERT INTO SalesOrderDetails (order_detail_id, order_id, product_id, quantity, price_for_product) VALUES
 (1, 1, 1, 2, 2400),  -- Laptop x2
 (2, 2, 2, 2, 1600),  -- Smartphone x2
@@ -680,7 +679,7 @@ INSERT INTO SalesOrderDetails (order_detail_id, order_id, product_id, quantity, 
 (115, 50, 13, 10, 500);  -- Electric Kettle x10
 
 
--- 插入订货单数据
+-- Insert PO
 INSERT INTO PurchaseOrders (po_id, supplier_id, order_date, expected_delivery_date, status, total_cost) VALUES
 (1, 1, '2024-01-10', '2024-01-20', 'Delivered', 34500),
 (2, 2, '2024-01-12', '2024-01-22', 'Delivered', 43000),
@@ -717,7 +716,7 @@ INSERT INTO PurchaseOrders (po_id, supplier_id, order_date, expected_delivery_da
 (33, 3, '2024-03-14', '2024-03-24', 'Delivered', 59500);
 
 
--- 插入每张订货单的细节数据
+-- Insert PO details
 INSERT INTO PurchaseOrderDetails (pod_id, po_id, catalog_id, quantity, cost_for_product) VALUES
 (1, 1, 1, 30, 34500),  -- Supplier 1, Laptop x30, cost: 1150 each
 (2, 2, 4, 40, 32000),  -- Supplier 2, Refrigerator x40, cost: 800 each
@@ -807,11 +806,9 @@ INSERT INTO PurchaseOrderDetails (pod_id, po_id, catalog_id, quantity, cost_for_
 (86, 33, 9, 30, 18750);-- Supplier 3, Digital Camera x30, cost: 625 each
 
 
--- 插入39条假数据到WarehouseTransfers表 liuyi
--- 清空已有的 WarehouseTransfers 数据  ----NOTE 如果准备在本地多次测试的话请酌情comment掉这条，可能造成数据清空
 TRUNCATE TABLE WarehouseTransfers;
 
--- 插入新的假数据
+-- Insert new warehouseTransfers
 INSERT INTO WarehouseTransfers (from_warehouse_id, to_warehouse_id, product_id, quantity, transfer_date, status) VALUES
 (1, 2, 1, 7, '2024-01-15', 'Completed'),
 (2, 3, 2, 5, '2024-01-18', 'Completed'),
